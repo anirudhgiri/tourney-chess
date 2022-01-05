14277655bee7009f784756c46d6cc965985c78f0
Anirudh Giri <anirudhgirimail@gmail.com>
2022-01-06 00:01:35 +0530

    add route to api and connection to pg db

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
+app.use(express.json());
+
+//import middleware for working with cookies
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
 
 //load environment variables from the .env file
 require("dotenv").config();
@@ -11,6 +16,7 @@ require("dotenv").config();
 //importing routes
 const homeRouter = require("./routes/home");
 const tournamentsRouter = require("./routes/tournaments");
+const apiRouter = require("./routes/api/api");
 
 //logging middleware
 const logger = require("morgan");
@@ -29,6 +35,7 @@ app.set("view engine", "hbs");
 //routing
 app.use("/", homeRouter);
 app.use("/tournaments", tournamentsRouter);
+app.use("/api", apiRouter);
 
 //catch error 404
 app.use((req,res) => {
@@ -52,6 +59,20 @@ app.use(function(err, req, res, _next) {
 	});
 });
 
-app.listen(process.env.PORT, ()=>{
-	console.log(`Listening on port ${process.env.PORT}...`);
-});
+const pg = require("./models/postgres");
+pg.sequelize
+	.authenticate()
+	.then(() => {
+		console.log("Postgres connection has been established successfully.");
+		app.emit("ready");
+	})
+	.catch(err => {
+		console.error("Unable to connect to the database:", err);
+	});
+
+//listen on http port when ready
+app.on("ready", () => {
+	app.listen(process.env.PORT, ()=>{
+		console.log(`Listening on port ${process.env.PORT}...`);
+	});
+})
