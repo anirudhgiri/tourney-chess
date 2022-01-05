0041840b0e75ddfced3ab9317948677520a4671e
Anirudh Giri <anirudhgirimail@gmail.com>
2022-01-05 19:25:43 +0530

    add title to homepage

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res)=> {
-	res.render("home", {homePage: true });
+	res.render("home", {homePage: true, title: "TourneyChess - Free Online Chess Tournament Manager" });
 });
 
 module.exports = router;
