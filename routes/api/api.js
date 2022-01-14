549990cec0d7d329ef68852edce31d1e30c1222c
Anirudh Giri <anirudhgirimail@gmail.com>
2022-01-15 04:56:43 +0530

    add api endpoint to handle tournaments

--- routes/api/api.js
+++ routes/api/api.js
@@ -3,8 +3,10 @@ const apiRouter = express.Router();
 
 const signupRouter = require("./signup");
 const loginRouter = require("./login");
+const tournamentsRouter = require("../../src/routes/tournaments");
 
 apiRouter.use("/signup", signupRouter);
 apiRouter.use("/login", loginRouter);
+apiRouter.use("/tournaments", tournamentsRouter)
 
 module.exports = apiRouter;
---
+++ routes/api/tournaments.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const tournamentRouter = express.Router();
+
+tournamentRouter.get('/', (req,res) => {
+
+});
