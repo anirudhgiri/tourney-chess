bfccdde131fb8a7c09b2209d098c3ed3b229b747
Anirudh Giri <anirudhgirimail@gmail.com>
2022-01-15 04:56:22 +0530

    Add topbar text and footer

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ const express = require("express");
 const homeRouter = express.Router();
 
 homeRouter.get("/", (req, res)=> {
-	res.render("home", {homePage: true, title: "TourneyChess - Free Online Chess Tournament Manager" });
+	res.render("home", {homePage: true, title: "TourneyChess - Free Online Chess Tournament Manager", topbarText: "TourneyChess - Free Online Chess Tournament Manager" });
 });
 
 module.exports = homeRouter;
--- routes/tournaments.js
+++ routes/tournaments.js
@@ -2,7 +2,7 @@ const express = require("express");
 const tournamentsRouter = express.Router();
 
 tournamentsRouter.get("/", (req, res) => {
-	res.render("tournaments",{tourneyPage: true, title:"Tournaments - TourneyChess"});
+	res.render("tournaments",{tourneyPage: true, title:"Tournaments - TourneyChess", topbarText: "My Tournaments"});
 });
 
 module.exports = tournamentsRouter;
