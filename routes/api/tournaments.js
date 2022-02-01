b85fc1a5337966f496438090846da57541643926
Anirudh Giri <anirudhgirimail@gmail.com>
2022-02-01 06:11:19 +0530

    create tournaments management endpoint

--- routes/api/tournaments.js
+++ routes/api/tournaments.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const tournamentRouter = express.Router();
 
-tournamentRouter.get('/', (req,res) => {
-
+tournamentRouter.get("/", (req,res) => {
+	res.json({success:true});
 });
+
+tournamentRouter.post("/createTournament", (req, res) => {
+	res.json({success:true});
+});
+
+module.exports = tournamentRouter;
+
