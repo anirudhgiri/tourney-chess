b39239ce4030c34acb5637df1b35e7e70505e4b9
Anirudh Giri <anirudhgirimail@gmail.com>
2022-02-13 17:32:36 +0530

    change code styling to match other models

--- models/userModel.js
+++ models/userModel.js
@@ -1,32 +1,13 @@
-const mongoose = require("mongoose");
+const {Schema, model} = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-	username: {
-		type: String,
-		required: true
-	},
-
-	email: {
-		type: String,
-		required: true
-	},
-
-	passwordHash: {
-		type: String,
-		required: true
-	},
-
-	passwordSalt: {
-		type: String,
-		required: true
-	},
-
-	joinDate: {
-		type: Date,
-		required: true
-	}
+const userSchema = new Schema({
+	username: {type: String, required: true},
+	email: {type: String, required: true},
+	passwordHash: {type: String, required: true},
+	passwordSalt: {type: String, required: true},
+	joinDate: {type: Date, required: true}
 });
 
-const User = mongoose.model("User", userSchema);
+const User = model("User", userSchema);
 
 module.exports = User;
