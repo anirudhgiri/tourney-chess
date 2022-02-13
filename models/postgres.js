74d7b8476fff3465e301466fd4e8a54ea5515714
Anirudh Giri <anirudhgirimail@gmail.com>
2022-02-13 17:21:15 +0530

    remove redis and pg connections

--- models/postgres.js
+++
@@ -1,8 +0,0 @@
-const {Sequelize} = require("sequelize");
-const sequelize = new Sequelize(process.env.PG_CONNECTION_URI);
-
-const pg = {};
-pg.Sequelize = Sequelize;
-pg.sequelize = sequelize;
-
-module.exports = pg;
--- models/redis.js
+++
@@ -1,5 +0,0 @@
-const Ioredis = require("ioredis");
-
-const redisClient = new Ioredis(process.env.REDIS_CONNECTION_URI+"?allowUsernameInURI=true");
-
-module.exports = redisClient;
