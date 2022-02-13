b8b502852f4b139ee30a353724df8e34a5e8d2d3
Anirudh Giri <anirudhgirimail@gmail.com>
2022-02-13 23:36:55 +0530

    add validations

--- models/userModel.js
+++ models/userModel.js
@@ -1,11 +1,20 @@
 const {Schema, model} = require("mongoose");
+const {nanoid} = require("nanoid/non-secure");
+
+function validateEmail(email) {
+	// eslint-disable-next-line no-useless-escape
+	let re = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+	return re.test(email);
+}
 
 const userSchema = new Schema({
-	username: {type: String, required: true},
-	email: {type: String, required: true},
+	id: {type: String, default: ()=> nanoid(6)},
+	username: {type: String, required: true, maxlength: 20},
+	email: {type: String, required: true, validate: [validateEmail, "Invalid email address format"], maxlength:320},
 	passwordHash: {type: String, required: true},
 	passwordSalt: {type: String, required: true},
-	joinDate: {type: Date, required: true}
+	joinDate: {type: Date, required: true},
+	tournaments: {type: [String], default: []}
 });
 
 const User = model("User", userSchema);
