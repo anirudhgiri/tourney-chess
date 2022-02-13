011efbef319fbfc9be9f649e26e20369d0168d98
Anirudh Giri <anirudhgirimail@gmail.com>
2022-02-13 23:35:11 +0530

    add rounds and games schemas

---
+++ models/gameModel.js
@@ -0,0 +1,13 @@
+const { Schema } = require("mongoose");
+const {nanoid} = require("nanoid/non-secure");
+
+const gameSchema = new Schema({
+	id: {type: String, default: ()=> nanoid(4)},
+	whitePlayer: {type: String, required: true},
+	blackPlayer: {type: String, required: true},
+	result: {type: String, default: ""},
+});
+
+module.exports = {
+	gameSchema
+};
---
+++ models/resultModel.js
@@ -0,0 +1,13 @@
+const { Schema } = require("mongoose");
+const {nanoid} = require("nanoid/non-secure");
+
+const resultSchema = new Schema({
+	id: {type: String, default: ()=> nanoid(4)},
+	colour: {type: String, required: true},
+	opponentID: {type: String, required: true},
+	points: {type: Number, required: true}
+});
+
+module.exports = {
+	resultSchema,
+};
